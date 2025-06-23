d558a199fe114bb0a473dcbec345dedff69de78a
Yohance Nayak <128442711+Yohands123@users.noreply.github.com>
2025-06-23 10:12:29 +0300

    Update ServerStore.cs

--- Data/ServerStore.cs
+++ Data/ServerStore.cs
@@ -1,16 +1,27 @@
-﻿using BlazorSSRApp.Components.Pages;
-using BlazorSSRApp.Models;
-using Microsoft.AspNetCore.Hosting.Server;
 using System.Collections.Generic;
+using System.Linq;            // for FirstOrDefault
+using BlazorSSRApp.Models;   // for your Server model
 
 namespace BlazorSSRApp.Data
 {
     public static class ServerStore
     {
-        public static List<Server> Servers { get; } = new()
+        public static List<Server> Servers { get; } = new List<Server>
         {
-            new Server (){ Name = "Server1", City = "Toronto", IsOnline = false, PeopleOnline = 0 },
-            new Server (){ Name = "Server2", City = "Toronto", IsOnline = false, PeopleOnline = 0 },
+            new Server { Id = 1, Name = "Server1", City = "Toronto", IsOnline = false, PeopleOnline = 0 },
+            new Server { Id = 2, Name = "Server2", City = "Toronto", IsOnline = false, PeopleOnline = 0 },
         };
+
+        public static void Update(Server server)
+        {
+            var db = Servers.FirstOrDefault(x => x.Id == server.Id);
+            if (db is not null)
+            {
+                db.Name = server.Name;
+                db.City = server.City;
+                db.IsOnline = server.IsOnline;
+                db.PeopleOnline = server.PeopleOnline;
+            }
+        }
     }
 }
